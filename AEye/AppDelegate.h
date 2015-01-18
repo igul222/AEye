349c49481a74eb4d63b664a874d02f38d3ff8ea8
Ishaan Gulrajani <igul222@gmail.com>
2015-01-17 22:43:06 -0500

    Initial Commit

---
+++ AEye/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AEye
+//
+//  Created by Ishaan Gulrajani on 1/17/15.
+//  Copyright (c) 2015 Ishaan Gulrajani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AEye/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AEye
+//
+//  Created by Ishaan Gulrajani on 1/17/15.
+//  Copyright (c) 2015 Ishaan Gulrajani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
